4465d431028a1fcf5cb0cbd6c8c7cf64faed936d
efetobore <39761576+efetobore@users.noreply.github.com>
2018-05-30 10:14:04 +0100

    Add files via upload

---
+++ Tobor Quadratic Equation.c
@@ -0,0 +1,32 @@
+/*NAME: MMADU EFETOBORE GREAT.
+MAT. NO.: COS/0048/2014
+ASSIGNMENT ON CSC414.
+*/
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+double a,b,c,root1,root2;
+float det;
+printf(" Please enter value for a \n");
+scanf("%lf",&a);
+printf(" Please enter value for b \n");
+scanf("%lf",&b);
+printf(" Please enter value for c \n");
+scanf("%lf",&c);
+det=(b*b)-(4.*a*c);
+if (det>0)
+{
+root1 = (-b + sqrt(det) ) / (2.*a);
+root2 = (-b - sqrt(det) ) / (2.*a);
+printf("\n First root is %lf ",root1);
+printf("\n Second root is %lf ",root2);
+}
+else
+{
+printf("\n Discriminant is negative!");
+}
+printf("\n ");
+return 0;
+}
+
